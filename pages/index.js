c8764244fd0dff9a1a7e58408aa0abe27baa3a57
viver77 <ya.ya-vs@yandex.ru>
2021-02-09 07:32:18 +0300

    09.02

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 
 //-------Область объявления переменных-------------------------------------//
 
-const $popup = document.querySelector('.popup')
-const $profileTitle = document.querySelector('.profile__title')
-const $profileSubTitle = document.querySelector('.profile__subtitle')
-const $popupHeader = document.querySelector('.popup__title')
-const $popupTitle   = document.querySelector('.popup__text_type_title')
-const $popupSubTitle   = document.querySelector('.popup__text_type_subtitle')
-const $editBtn =  document.querySelector('.profile__edit-btn')
-const $addBtn = document.querySelector('.profile__add-btn')
-const $formElement = document.querySelector('.popup__container')
-const $elements = document.querySelector('.elements')
-const $cardTemplate = document.querySelector('#card').content
-const $popupSubmit = document.querySelector('.popup__submit-btn')
-const $popupImage = document.querySelector('.popup-image')
-const $popupImageImage = document.querySelector('.popup-image__image')
-const $popupClose = document.querySelectorAll('.popup__close-btn')
-const $popupImageFigcaption = document.querySelector('.popup-image__figcaption')
+const profileTitle = document.querySelector('.profile__title')
+const profileSubTitle = document.querySelector('.profile__subtitle')
+const formTitle   = document.querySelector('.form__text_type_title')
+const formSubTitle   = document.querySelector('.form__text_type_subtitle')
+const editBtn =  document.querySelector('.profile__edit-btn')
+const addBtn = document.querySelector('.profile__add-btn')
+const popupCloseBtn = document.querySelectorAll('.popup__close-btn')
+const elements = document.querySelector('.elements')
+const cardTemplate = document.querySelector('.template-card')
+const popupImage = document.querySelector('.popup-image')
+const popupImageImage = document.querySelector('.popup-image__image')
+const popupImageFigcaption = document.querySelector('.popup-image__figcaption')
+const popupEdit = document.querySelector('.popup-edit')
+const popupAdd = document.querySelector('.popup-add')
+const formEdit = document.querySelector('.form-edit')
+const formAdd = document.querySelector('.form-add')
 
 const initialCards = [
     {
@@ -47,111 +47,96 @@ const initialCards = [
 
 //-------Область объявления функций-------------------------------------//
 
-function closePopup(currentTarget) {
-    if (currentTarget.parentElement.classList.contains('popup-image__figure')) {
-        $popupImage.classList.remove('popup_opened')
-    }
-    else {
-        $popup.classList.remove('popup_opened')
-    }
+function closePopup(popup) {
+    popup.classList.remove('popup_opened')
 }
 
-function openPopup() {
-    $popup.classList.add('popup_opened')
+function openPopup(popup) {
+    popup.classList.add('popup_opened')
 }
 
-function addCard(item) {
+function getNewCard(item) {
 
-    const $card = $cardTemplate.querySelector('.card').cloneNode(true)
-    const $cardImage = $card.querySelector('.card__image')
-    const $cardDescription = $card.querySelector('.card__description')
+    const newCard = cardTemplate.content.cloneNode(true)
+    const cardImage = newCard.querySelector('.card__image')
+    const cardDescription = newCard.querySelector('.card__description')
 
-    $cardImage.src = item.link
-    $cardImage.alt = item.name
+    cardImage.src = item.link
+    cardImage.alt = item.name
+    cardDescription.textContent = item.name
 
-    $cardDescription.textContent = item.name
+    newCard.querySelector('.card__like').addEventListener('click', evt => {
+        evt.target.classList.toggle('card__like_active')
+    })
 
-    $elements.prepend($card)
+    newCard.querySelector('.card__trash').addEventListener('click', evt => {
+        evt.currentTarget.parentElement.remove()
+    })
+
+    cardImage.addEventListener('click', evt => {
+
+        popupImageImage.src = evt.currentTarget.src
+        popupImageImage.alt = evt.currentTarget.alt
+        popupImageFigcaption.textContent = evt.currentTarget.alt
+
+        openPopup(popupImage)
+    })
+
+    return newCard
 }
 
-//-------Основная область-------------------------------------//
-
-//------- Заполнить и открыть карточку профиля --------------//
-$editBtn.addEventListener("click", () => {
-
-    $popupHeader.textContent = 'Редактировать профиль'
-    $popupTitle.placeholder = 'Здесь нужно ввести имя'
-    $popupSubTitle.placeholder = 'Здесь нужно ввести описание'
-    $popupSubmit.textContent = 'Сохранить'
-
-    $popupTitle.value = $profileTitle.textContent
-    $popupSubTitle.value = $profileSubTitle.textContent
-
-    openPopup();
-})
-
-//------- Добавить новую карточку --------------//
-$addBtn.addEventListener('click', evt => {
-
-    $popupHeader.textContent = 'Новое место'
-    $popupTitle.placeholder = 'Название'
-    $popupSubTitle.placeholder = 'Ссылка на картинку'
-    $popupSubmit.textContent = 'Создать'
-
-    $popupTitle.value = ''
-    $popupSubTitle.value = ''
-
-    openPopup();
-})
-
-
-//-------Отправить данные на сервер-----------------------------//
-$formElement.addEventListener('submit', evt => {
-    evt.preventDefault();
-
-    if ($popupHeader.textContent === 'Редактировать профиль') {
-
-    $profileTitle.textContent = $popupTitle.value
-    $profileSubTitle.textContent = $popupSubTitle.value}
-
-    if ($popupHeader.textContent === 'Новое место') {
-
-        addCard({
-            name: $popupTitle.value,
-            link: $popupSubTitle.value
-        })
-    }
-
-    closePopup(evt.currentTarget);
-});
+//-------Инициализация данных-------------------------------------//
 
 initialCards.forEach(item => {
-    addCard(item)
+    elements.append(getNewCard(item))
 })
 
+//------- Обработчики событий форм------------------------------//
 
-//------- Добавить обработчик на кнопки закрытия popup --------------//
-$popupClose.forEach(item => {
+formEdit.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    profileTitle.textContent = evt.currentTarget.querySelector('.form__text_type_title').value
+    profileSubTitle.textContent = evt.currentTarget.querySelector('.form__text_type_subtitle').value
+
+    closePopup(popupEdit);
+});
+
+formAdd.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const formTitle = evt.currentTarget.querySelector('.form__text_type_title')
+    const formSubtitle = evt.currentTarget.querySelector('.form__text_type_subtitle')
+
+    elements.prepend(getNewCard(
+        {
+            name: formTitle.value,
+            link: formSubtitle.value
+        }
+    ))
+
+    closePopup(popupAdd);
+
+    formTitle.value = ''
+    formSubtitle.value = ''
+
+});
+
+//------- Обработчики событий кнопок------------------------------//
+editBtn.addEventListener("click", () => {
+
+    formTitle.value = profileTitle.textContent
+    formSubTitle.value = profileSubTitle.textContent
+
+    openPopup(popupEdit);
+})
+
+addBtn.addEventListener('click', evt => {
+    openPopup(popupAdd);
+})
+
+popupCloseBtn.forEach(item => {
     item.addEventListener('click', evt => {
-        closePopup(evt.currentTarget)
+        closePopup(evt.currentTarget.parentElement.parentElement)
     })
-})
-
-//------- Обработка событий карточки --------------//
-$elements.addEventListener('click', evt => {
-    if (evt.target.classList.contains('card__like')) {
-        evt.target.classList.toggle('card__like_active')
-    }
-
-    if (evt.target.classList.contains('card__trash')) {
-         evt.target.parentElement.remove()
-    }
-
-    if (evt.target.classList.contains('card__image')) {
-        $popupImageImage.src = evt.target.src
-        $popupImageFigcaption.textContent = evt.target.alt
-
-        $popupImage.classList.add('popup_opened')
-    }
-
 })
